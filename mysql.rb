a5289a047316719b28709ec35d1838a4cfad5f20
Danny W <teeigeryuh@yahoo.com>
2021-10-29 00:02:25 +0000

    q fix

--- mysql.rb
+++ mysql.rb
@@ -1,5 +1,9 @@
 require 'inifile'
 
+# install mariadb = "sudo apt-get install mariadb"
+# secure_install = "sudo mysql_secure_installation"
+# ruby deps = "sudo apt-get install ruby-mysql2 default-libmysqlclient-dev && gem install mysql2"
+
 client = Mysql2::Client.new(
   :host => "#{settingini['db']['host']}",
   :username => "#{settingini['db']['username']}",
@@ -7,22 +11,13 @@ client = Mysql2::Client.new(
   :password => "#{settingini['db']['password']}"
 )
 
-client.query("")
-
-
-install mariadb = "sudo apt-get install mariadb"
-secure_install = "sudo mysql_secure_installation"
-ruby deps = "sudo apt-get install ruby-mysql2 default-libmysqlclient-dev && gem install mysql2"
-reset_root_pass = ""
-
 def reset_root (new_password)
 	stop_mariadb = `sudo systemctl stop mariadb`
 	start_mariadb = `sudo mysqld_safe --skip-grant-tables --skip-networking &`
 
 	reset_password = "mysql -u root \
-  "UPDATE mysql.user SET authentication_string = PASSWORD('Nopasswordforniggers@') WHERE User = 'root' AND Host = 'localhost';""
+  "UPDATE mysql.user SET authentication_string = PASSWORD('Passwordexample4u') WHERE User = 'root' AND Host = 'localhost';""
 end
-
 
 def create_mysql_user (username, password)
 	client.query("CREATE USER '#{username}'@'localhost' IDENTIFIED BY '#{password}';")
